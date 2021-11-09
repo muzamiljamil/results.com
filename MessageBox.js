2d623df54dedfbb2875f01f98477768edca50143
muzamil26 <81831067+muzamiljamil@users.noreply.github.com>
2021-11-09 12:53:20 -0800

    Add files via upload

---
+++ MessageBox.js
@@ -0,0 +1,22 @@
+﻿function pageLoad() {
+    //alert("pageLoad");
+    // Initialization code here, meant to run once. 
+    $(document).ready(function () {
+        
+        setTimeout(function () {
+            $("#ctl00_MsgBox_MessageContainer").fadeOut("slow", function () {
+                $("#ctl00_MsgBox_MessageContainer").remove();
+            });
+        }, 90000);
+        $('.closeImage').click(function () {
+            $(this).parent().hide();
+            alert("class close Image");
+        });
+
+        $('#MsgBox_CloseImage').click(function () {
+            $(this).parent().hide();
+            alert("ID close Image");
+        });
+    });
+}
+
